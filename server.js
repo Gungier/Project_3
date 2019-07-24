c3d8a2fed032656db91b48dd6e4ae16e36ea40fa
Gabe <gungier1@gmail.com>
2019-07-24 17:20:01 -0400

    modified server

--- server.js
+++ server.js
@@ -2,25 +2,13 @@ var express = require("express")
 var cors = require("cors")
 var bodyParser = require("body-parser")
 var app = express()
+var path = require("path")
 var port = process.env.PORT || 3001
 // var db = require("./models")
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-//     app.get("/*", function(req, res) {
-//       res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//     });
-//   }
-  
-//   else {
-//     app.use(express.static(path.join(__dirname, '/client/public')));
-//     app.get("/*", function(req, res) {
-//       res.sendFile(path.join(__dirname, "/client/public/index.html"));
-//     });
-//   }
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -33,10 +21,23 @@ var Currencies = require("./routes/Currencies")
 
 app.use("/users", Users)
 app.use("/currencies", Currencies)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
 
 // db.sequelize.sync().then(function() {
     app.listen(port, function() {
       console.log("App now listening on port:", port);
     });
-//   
+
   
