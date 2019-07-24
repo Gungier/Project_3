be58c730a823ed9bb969646f237344d1fb5f00fd
Gabe <gungier1@gmail.com>
2019-07-24 18:33:25 -0400

    another tweak to server

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ var cors = require("cors")
 var bodyParser = require("body-parser")
 var app = express()
 var path = require("path")
-var port = process.env.PORT || 3001
+var PORT = process.env.PORT || 3001
 // var db = require("./models")
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+  }
+  
+  else {
+    app.use(express.static(path.join(__dirname+ '/client/public')));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname+ "/client/public/index.html"));
+    });
+  }
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -19,40 +33,14 @@ app.use(bodyParser.urlencoded({
 var Users = require("./routes/Users")
 var Currencies = require("./routes/Currencies")
 
-app.use("/users", Users);
-app.use("/currencies", Currencies);
+app.use("/users", Users)
+app.use("/currencies", Currencies)
 
-var mysql = require('mysql');
-var connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password123',
-    database: 'currency_tracker'
-  });
-};
-connection.connect();
-
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// db.sequelize.sync({
+//   force: false }).then(function () {
+  
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
   });
 
-
-// else {
-//   app.use(express.static(path.join(__dirname, '/client/public')));
-//   app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./client/public/index.html"));
-//   });
-// 
-
-// db.sequelize.sync().then(function() {
-    app.listen(port, function() {
-      console.log("App now listening on port:", port);
-    });
-
-  module.exports = connection;
+// });
