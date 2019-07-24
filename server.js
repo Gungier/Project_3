d8d9cf36b181c99a3941102218a7e6c4c33469d9
Gabe <gungier1@gmail.com>
2019-07-24 18:05:13 -0400

    server again

--- server.js
+++ server.js
@@ -19,9 +19,24 @@ app.use(bodyParser.urlencoded({
 var Users = require("./routes/Users")
 var Currencies = require("./routes/Currencies")
 
-app.use("/users", Users)
-app.use("/currencies", Currencies)
-if (process.env.NODE_ENV === "production") {
+app.use("/users", Users);
+app.use("/currencies", Currencies);
+
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password123',
+    database: 'currency_tracker'
+  });
+};
+connection.connect();
+
   app.use(express.static("client/build"));
   app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
@@ -40,4 +55,4 @@ else {
       console.log("App now listening on port:", port);
     });
 
-  
+  module.exports = connection;
