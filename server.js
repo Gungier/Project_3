83ad14a0501afc727c9a9bd092a53feff24f1d07
Gabe <gungier1@gmail.com>
2019-07-25 11:56:27 -0400

    a

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ else {
 db.sequelize.sync({
   force: false }).then(function () {
     
-    app.listen(process.env.PORT || 3000, function(){
-      console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-    });
+  app.listen(process.env.PORT || 3001, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 });
