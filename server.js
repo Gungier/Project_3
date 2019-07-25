8994d0280475d810a9eb5da4dfa1d35c5db7601b
Gabe <gungier1@gmail.com>
2019-07-25 11:54:15 -0400

    trying more dynamic server approach

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ else {
 db.sequelize.sync({
   force: false }).then(function () {
     
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(process.env.PORT || 3000, function(){
+      console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    });
 
 });
